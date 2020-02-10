ad3497e0f778e458f9e8a5fb6abb02f1ea336efd
Sefa Taşcan <sefatascan@hotmail.com>
2020-02-10 18:23:21 +0300

    data sql file for starting application

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,15 @@
-insert into FANS_OF_TEAM values (1, '1994-11-13', 'Galatasaray', 'taraftar', 1, 'Sefa', 'Ahmet', 'Yılmaz', 'Taşcan');
+insert into FANS_OF_TEAM values (1, '1978-05-16', 'Galatasaray', 'Destekçi', 1, 'Ali', 'Ahmet', 'Yılmaz', 'Öztürk');
+insert into FANS_OF_TEAM values (2, '1999-11-25', 'Fenerbahçe', 'Taraftar', 0, 'Zeynep', 'Ahmet', 'Yılmaz', 'Karaduman');
+insert into FANS_OF_TEAM values (3, '1964-01-10', 'Beşiktaş', 'Fanatik', 1, 'Mustafa', 'Fatih', 'Kocaoğlu', 'Ayka');
+insert into FANS_OF_TEAM values (4, '1984-04-30', 'Galatasaray', 'Fanatik', 1, 'Aslan', 'Ahmet', 'Yılmaz', 'Güçlü');
+insert into FANS_OF_TEAM values (5, '1993-11-13', 'Kayserispor', 'Destekçi', 0, 'Cansu', 'Ahmet', 'Yılmaz', 'Biberci');
+insert into FANS_OF_TEAM values (6, '1988-02-05', 'Trabzonspor', 'Fanatik', 1, 'Efe', 'Şeyma', 'Özcan', 'Karabulut');
+insert into FANS_OF_TEAM values (7, '1972-07-08', 'Belediye Spor', 'Destekçi', 1, 'Erman', 'Fatih', 'Kocaoğlu', 'Yurt');
+
+insert into LIFE_QUALITY values (1, '1972-07-08', 'Stresli', 1, 'Yüzme', 4, 'Erman', 'Fatih', 'Kocaoğlu', 'Yurt', 'Okumak');
+insert into LIFE_QUALITY values (2, '1978-05-16', 'Hayat dolu', 1, 'Kayak', 9, 'Ali', 'Ahmet', 'Yılmaz', 'Öztürk', 'Koşu');
+insert into LIFE_QUALITY values (3, '1999-11-25', 'Neşeli', 0, 'Futbol', 7, 'Zeynep', 'Ahmet', 'Yılmaz', 'Karaduman', 'Yüzme');
+insert into LIFE_QUALITY values (4, '1964-01-10', 'Depresif', 1, null , 2, 'Mustafa', 'Fatih', 'Kocaoğlu', 'Ayka', 'Hiçbir şey');
+insert into LIFE_QUALITY values (5, '1984-04-30', 'Normal', 1, 'Yürüyüş', 6, 'Aslan', 'Ahmet', 'Yılmaz', 'Güçlü', 'Okumak');
+insert into LIFE_QUALITY values (6, '1993-11-13', 'Neşeli', 0, 'Yürüyüş', 8, 'Cansu', 'Ahmet', 'Yılmaz', 'Biberci', 'Koşu');
+insert into LIFE_QUALITY values (7, '1988-02-05', 'Normal', 1, 'Yüzme', 6, 'Efe', 'Şeyma', 'Özcan', 'Karabulut', 'Yürüyüş');
